b87e7a3ecea25c49edeb3f879388fc9e50e1104b
NadezhdaTA <nadezda.tupitsyna@yandex.ru>
2026-03-19 12:02:48 +0400

    Initial commit

---
+++ src/main/java/com/example/astonconfigservice/AstonConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.astonconfigservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstonConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstonConfigServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/astonconfigservice/AstonConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.astonconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstonConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
