36a6e6bb9916ab7e3acc21fe557b4a6768848883
NadezhdaTA <nadezda.tupitsyna@yandex.ru>
2026-03-23 19:57:12 +0400

    Dockerfile

--- src/main/java/com/example/astonconfigservice/AstonConfigServiceApplication.java
+++ src/main/java/com/example/astonconfigservice/AstonConfigServiceApplication.java
@@ -2,12 +2,14 @@ package com.example.astonconfigservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class AstonConfigServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AstonConfigServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AstonConfigServiceApplication.class, args);
+    }
 
 }
